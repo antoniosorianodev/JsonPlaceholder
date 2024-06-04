e7d4af46c6607e71b505c8336c703f2086affa9b
Antonio <asoriano@appdev.yearup.org>
2024-06-04 12:15:40 -0500

    added nav bar and message generation based on user input

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,5 +1,22 @@
 "use strict"
 
 window.onload = () => {
+    let myDiv = document.querySelector("#results");
+    let myButton = document.querySelector("#theButton");
+    let myInput = document.querySelector("#todoId")
 
+    myButton.addEventListener("click", () => daFunction(myInput, myDiv));
 }
+
+async function daFunction(inputField, outputField) {
+    let url = "https://jsonplaceholder.typicode.com/todos/" + inputField.value;
+    let response = await fetch(url, {});
+    let data = await response.json();
+
+    outputField.innerHTML = `
+    userId: ${data.userId}
+    id: ${data.id}
+    title: ${data.title}
+    completed: ${data.completed}
+    `;
+} 
