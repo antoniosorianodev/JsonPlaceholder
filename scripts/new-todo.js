cdb4da7d26f5073706e22cd02386252d4580124a
Antonio <asoriano@appdev.yearup.org>
2024-06-11 14:43:39 -0500

    added a new todo page with POST functionality

---
+++ scripts/new-todo.js
@@ -0,0 +1,31 @@
+"use strict"
+
+window.onload = () => {
+    let todoForm = document.querySelector("#todoForm");
+
+    todoForm.addEventListener("submit", createATodo);
+}
+
+const createATodo = async (event) => {
+    event.preventDefault();
+
+    let formData = new FormData(event.target);
+    let formDataAsObject = Object.fromEntries(formData);
+
+    try {
+
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+            body: JSON.stringify(formDataAsObject)
+        });
+
+        let newTodo = await response.json();
+
+        if (response.ok) {
+            alert(`Success! Posted with id: ${newTodo.id}`);
+        }
+    } catch (error) {
+        console.log("something went wrong", error);
+    }
+}
